5b8f3f10795f2b5bc7b11c9c074d621351cc4522
Joshua Peek <josh@joshpeek.com>
2011-07-07 11:22:50 -0500

    Test more bad UAs

--- spec/browsers/other_user_agent_spec.rb
+++ spec/browsers/other_user_agent_spec.rb
@@ -80,3 +80,39 @@ describe "UserAgent: 'Rails Testing'" do
   it { @useragent.os.should be_nil }
   it { @useragent.should_not be_mobile }
 end
+
+describe "UserAgent: 'Python-urllib/2.7'" do
+  before do
+    @useragent = UserAgent.parse("Python-urllib/2.7")
+  end
+
+  it "should return 'Python-urllib' as its browser" do
+    @useragent.browser.should == "Python-urllib"
+  end
+
+  it "should return '2.7' as its version" do
+    @useragent.version.should == "2.7"
+  end
+
+  it { @useragent.platform.should be_nil }
+  it { @useragent.os.should be_nil }
+  it { @useragent.should_not be_mobile }
+end
+
+describe "UserAgent: 'check_http/v1.4.15 (nagios-plugins 1.4.15)'" do
+  before do
+    @useragent = UserAgent.parse("check_http/v1.4.15 (nagios-plugins 1.4.15)")
+  end
+
+  it "should return 'check_http' as its browser" do
+    @useragent.browser.should == "check_http"
+  end
+
+  it "should return 'v1.4.15' as its version" do
+    @useragent.version.should == "v1.4.15"
+  end
+
+  it { @useragent.platform.should be_nil }
+  it { @useragent.os.should be_nil }
+  it { @useragent.should_not be_mobile }
+end
