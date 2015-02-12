f2845d211d6dca707f220c6b5803973e1cfc8ea5
Nik Wakelin <me@nikwakelin.com>
2015-02-12 14:41:30 +0000

    Adds ',gzip(gfe)' Literal To Comment Matcher

--- lib/user_agent.rb
+++ lib/user_agent.rb
@@ -6,9 +6,9 @@ require 'user_agent/version'
 class UserAgent
   # http://www.texsoft.it/index.php?m=sw.php.useragent
   MATCHER = %r{
-    ^([^/\s]+)        # Product
-    /?([^\s]*)        # Version
-    (\s\(([^\)]*)\))? # Comment
+    ^([^/\s]+)                     # Product
+    /?([^\s,]*)                    # Version
+    (\s\(([^\)]*)\)|,gzip\(gfe\))? # Comment
   }x.freeze
 
   DEFAULT_USER_AGENT = "Mozilla/4.0 (compatible)"
