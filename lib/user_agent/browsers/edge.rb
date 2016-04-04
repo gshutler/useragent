ead3f4db33586a68cb8620f2170969f9dcdac56d
Garry Shutler <garry@robustsoftware.co.uk>
2016-04-04 08:52:28 +0100

    Check all comments for Edge OS
    Previously the code assumed the first agent would contain the OS
    information. However, there are examples of this not being the case, and
    this would result in a NoMethodError.
    
    Resolves #32

--- lib/user_agent/browsers/edge.rb
+++ lib/user_agent/browsers/edge.rb
@@ -1,6 +1,8 @@
 class UserAgent
   module Browsers
     class Edge < Base
+      OS_REGEXP = /Windows NT [\d\.]+|Windows Phone (OS )?[\d\.]+/
+
       def self.extend?(agent)
         agent.last && agent.last.product == "Edge"
       end
@@ -18,7 +20,19 @@ class UserAgent
       end
 
       def os
-        OperatingSystems.normalize_os(application.comment.join('; ').match(/Windows NT [\d\.]+|Windows Phone (OS )?[\d\.]+/).to_s)
+        OperatingSystems.normalize_os(os_comment)
+      end
+
+      private
+
+      def os_comment
+        if os_agent = detect { |useragent| comment_os_match(useragent) }
+          comment_os_match(os_agent).to_s
+        end
+      end
+
+      def comment_os_match(useragent)
+        useragent.comment && useragent.comment.join("; ").match(OS_REGEXP)
       end
     end
   end
