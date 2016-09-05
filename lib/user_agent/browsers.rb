a8579497c8fc18dbfeffcdfcec48108825cc3de6
Garry Shutler <garry@robustsoftware.co.uk>
2016-09-05 11:42:09 +0200

    Check WeChat as low as possible

--- lib/user_agent/browsers.rb
+++ lib/user_agent/browsers.rb
@@ -1,11 +1,11 @@
 require 'user_agent/browsers/base'
-require 'user_agent/browsers/wechat_browser'
 require 'user_agent/browsers/chrome'
 require 'user_agent/browsers/edge'
 require 'user_agent/browsers/gecko'
 require 'user_agent/browsers/internet_explorer'
 require 'user_agent/browsers/opera'
 require 'user_agent/browsers/webkit'
+require 'user_agent/browsers/wechat_browser'
 require 'user_agent/browsers/windows_media_player'
 require 'user_agent/browsers/itunes'
 require 'user_agent/browsers/apple_core_media'
@@ -22,10 +22,10 @@ class UserAgent
     }.freeze
 
     ALL = [
-      WechatBrowser,
       Edge,
       InternetExplorer,
       Opera,
+      WechatBrowser,
       Chrome,
       ITunes,
       PlayStation,
