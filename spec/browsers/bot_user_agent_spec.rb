e6e21376d5c202cd973beee5fe213463142d2c40
Garry Shutler <garry@robustsoftware.co.uk>
2018-01-12 09:11:01 +0000

    Removed trailing whitespace

--- spec/browsers/bot_user_agent_spec.rb
+++ spec/browsers/bot_user_agent_spec.rb
@@ -36,6 +36,6 @@ describe "UserAgent: Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) A
   before do
     @useragent = UserAgent.parse("Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
   end
-  
+
   it { expect(@useragent).to be_bot}
 end
