be60b6150cd72470149983182d8a485b9a1ad346
SiuKit Wong <skwongr@gmail.com>
2018-05-17 19:04:58 -0500

    AROR-341 support NexPlayer and LG Player as well

--- lib/user_agent/browsers/stagefright.rb
+++ lib/user_agent/browsers/stagefright.rb
@@ -4,41 +4,76 @@ class UserAgent
     # SonyD5803 Build/23.0.1.A.5.77 stagefright/1.2 (Linux;Android 4.4.4)
     # Samsung SAMSUNG-SGH-I747 stagefright/1.2 (Linux;Android 4.4.2)
     # Samsung SM-G900T stagefright/Beyonce/1.1.9 (Linux;Android 5.1.1)
+    # AlcatelOneTouch-Alcatel_5044R-SVN/02; stagefright/1.2 (Linux;Android 7.0)
+    # BLADE L111-stagefright/1.2 (Linux;Android 5.1)
+    # Fire OS/5.1.1 stagefright/1.2 (Linux;Android 5.1.1) ,Dalvik/2.1.0 (Linux; U; Android 5.1.1; KFFOWI Build/LMY47O)
+    # Fire OS/6.0 stagefright/1.2 (Linux;Android 7.1.2)
+    # stagefright/1.2 (Linux;Android 5.0 Huawei HUAWEI GRA-L09 GRA-L09V100R001C150B134)
+    # stagefright/1.2 (Linux;Android 4.0.3) Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10
+
+    # LG-E615f/LG-E615f-V10p-SEP-02-2013 Player/NexPlayer 4.0 for Android (stagefright alternative) ,Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36
+    # LG-D802/Desperate Rom LP UX4 V4 Player/LG Player 1.0 for Android 5.0.2 (stagefright alternative)
+    # LG-H873/V15c Player/LG Player 1.0 for Android 7.0 (stagefright alternative)
+    # LG-H955/V15c Player/LG Player 1.0 for Android 5.1.1 (stagefright alternative) BMID/E67AB269FB
     class Stagefright < Base
       def self.extend?(agent)
-        agent.detect { |useragent| useragent.product =~ /stagefright/ }
+        agent.detect { |useragent| useragent.product =~ /stagefright/ } || agent.to_s =~ /stagefright alternative/
       end
 
       def browser
-        'Stagefright'
+        ua = self.to_s
+        if pos = ua =~ /NexPlayer/
+          return 'NexPlayer'
+        elsif pos = ua =~ /LG Player/
+          return 'LG Player'
+        end
+
+        if application && application.version
+          version = application.version.to_s
+          version.index('/') ? 'Stagefright/' + version.split('/')[0] : 'Stagefright'
+        end
       end
 
       # Find the right application
       def application
-        self.reject { |agent| agent.comment.nil? || agent.comment.empty? }.first
+        self.reject { |agent| !(agent.product =~ /stagefright/) || agent.comment.nil? || agent.comment.empty? }.first
       end
 
       # Return the platform
       #
       # @return [String]
       def platform
-        return unless application
-
-        if application.comment.any? { |c| c =~ /Android/ }
-          'Android'
-        else
-          application.comment[0]
-        end
+        'Android' if self.to_s =~ /Android/
       end
 
       # Return the operating system
       #
       # @return [String]
       def os
-        return unless application
+        if application && pos = application.comment[0] =~ /Android/
+          return OperatingSystems.normalize_os(application.comment[0][pos..-1].split()[0..1].join(' '))
+        end
 
-        if pos = application.comment[0] =~ /Android/
-          OperatingSystems.normalize_os(application.comment[0][pos..-1])
+        ua = self.to_s
+        if pos = ua =~ /Android/
+          OperatingSystems.normalize_os(ua[pos..-1].split()[0..1].join(' '))
+        end
+      end
+
+      # Return the application version if any or the player version
+      #
+      # @return [String]
+      def version
+        if application && application.version
+          version = application.version.to_s
+          return version.index('/') ? version.split('/')[-1] : application.version
+        end
+
+        ua = self.to_s
+        if pos = ua =~ /NexPlayer/
+          ua[pos..-1].split()[1]
+        elsif pos = ua =~ /LG Player/
+          ua[pos..-1].split()[2]
         end
       end
     end
--- spec/browsers/stagefright_user_agent_spec.rb
+++ spec/browsers/stagefright_user_agent_spec.rb
@@ -6,6 +6,24 @@ shared_examples 'Stagefright' do
   end
 end
 
+shared_examples 'Stagefright/Beyonce' do
+  it "returns 'Stagefright/Beyonce' as its browser" do
+    expect(@useragent.browser).to eq('Stagefright/Beyonce')
+  end
+end
+
+shared_examples 'NexPlayer' do
+  it "returns 'NexPlayer' as its browser" do
+    expect(@useragent.browser).to eq('NexPlayer')
+  end
+end
+
+shared_examples 'LG Player' do
+  it "returns 'LG Player' as its browser" do
+    expect(@useragent.browser).to eq('LG Player')
+  end
+end
+
 describe "UserAgent: stagefright/1.2 (Linux;Android 5.0.1)" do
   before do
     @useragent = UserAgent.parse("stagefright/1.2 (Linux;Android 5.0.1)")
@@ -71,10 +89,206 @@ describe "UserAgent: Samsung SM-G900T stagefright/Beyonce/1.1.9 (Linux;Android 5
     @useragent = UserAgent.parse("Samsung SM-G900T stagefright/Beyonce/1.1.9 (Linux;Android 5.1.1)")
   end
 
+  it_behaves_like 'Stagefright/Beyonce'
+
+  it "returns '1.1.9' as its version" do
+    expect(@useragent.version).to eq('1.1.9')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+
+  it "returns 'Android 5.1.1' as its operating system" do
+    expect(@useragent.os).to eq('Android 5.1.1')
+  end
+end
+
+describe "UserAgent: AlcatelOneTouch-Alcatel_5044R-SVN/02; stagefright/1.2 (Linux;Android 7.0)" do
+  before do
+    @useragent = UserAgent.parse("AlcatelOneTouch-Alcatel_5044R-SVN/02; stagefright/1.2 (Linux;Android 7.0)")
+  end
+
   it_behaves_like 'Stagefright'
 
-  it "returns 'Beyonce/1.1.9' as its version" do
-    expect(@useragent.version).to eq('Beyonce/1.1.9')
+  it "returns '1.2' as its version" do
+    expect(@useragent.version).to eq('1.2')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+
+  it "returns 'Android 7.0' as its operating system" do
+    expect(@useragent.os).to eq('Android 7.0')
+  end
+end
+
+describe "UserAgent: BLADE L111-stagefright/1.2 (Linux;Android 5.1)" do
+  before do
+    @useragent = UserAgent.parse("BLADE L111-stagefright/1.2 (Linux;Android 5.1)")
+  end
+
+  it_behaves_like 'Stagefright'
+
+  it "returns '1.2' as its version" do
+    expect(@useragent.version).to eq('1.2')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+
+  it "returns 'Android 5.1' as its operating system" do
+    expect(@useragent.os).to eq('Android 5.1')
+  end
+end
+
+describe "UserAgent: Fire OS/5.1.1 stagefright/1.2 (Linux;Android 5.1.1) ,Dalvik/2.1.0 (Linux; U; Android 5.1.1; KFFOWI Build/LMY47O)" do
+  before do
+    @useragent = UserAgent.parse("Fire OS/5.1.1 stagefright/1.2 (Linux;Android 5.1.1) ,Dalvik/2.1.0 (Linux; U; Android 5.1.1; KFFOWI Build/LMY47O)")
+  end
+
+  it_behaves_like 'Stagefright'
+
+  it "returns '1.2' as its version" do
+    expect(@useragent.version).to eq('1.2')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+
+  it "returns 'Android 5.1.1' as its operating system" do
+    expect(@useragent.os).to eq('Android 5.1.1')
+  end
+end
+
+describe "UserAgent: Fire OS/6.0 stagefright/1.2 (Linux;Android 7.1.2)" do
+  before do
+    @useragent = UserAgent.parse("Fire OS/6.0 stagefright/1.2 (Linux;Android 7.1.2)")
+  end
+
+  it_behaves_like 'Stagefright'
+
+  it "returns '1.2' as its version" do
+    expect(@useragent.version).to eq('1.2')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+
+  it "returns 'Android 7.1.2' as its operating system" do
+    expect(@useragent.os).to eq('Android 7.1.2')
+  end
+end
+
+describe "UserAgent: stagefright/1.2 (Linux;Android 5.0 Huawei HUAWEI GRA-L09 GRA-L09V100R001C150B134)" do
+  before do
+    @useragent = UserAgent.parse("stagefright/1.2 (Linux;Android 5.0 Huawei HUAWEI GRA-L09 GRA-L09V100R001C150B134)")
+  end
+
+  it_behaves_like 'Stagefright'
+
+  it "returns '1.2' as its version" do
+    expect(@useragent.version).to eq('1.2')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+
+  it "returns 'Android 5.0' as its operating system" do
+    expect(@useragent.os).to eq('Android 5.0')
+  end
+end
+
+describe "UserAgent: stagefright/1.2 (Linux;Android 4.0.3) Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10" do
+  before do
+    @useragent = UserAgent.parse("stagefright/1.2 (Linux;Android 4.0.3) Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10")
+  end
+
+  it_behaves_like 'Stagefright'
+
+  it "returns '1.2' as its version" do
+    expect(@useragent.version).to eq('1.2')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+
+  it "returns 'Android 4.0.3' as its operating system" do
+    expect(@useragent.os).to eq('Android 4.0.3')
+  end
+end
+
+describe "UserAgent: LG-E615f/LG-E615f-V10p-SEP-02-2013 Player/NexPlayer 4.0 for Android (stagefright alternative) ,Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36" do
+  before do
+    @useragent = UserAgent.parse("LG-E615f/LG-E615f-V10p-SEP-02-2013 Player/NexPlayer 4.0 for Android (stagefright alternative) ,Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36")
+  end
+
+  it_behaves_like 'NexPlayer'
+
+  it "returns '4.0' as its version" do
+    expect(@useragent.version).to eq('4.0')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+end
+
+describe "UserAgent: LG-D802/Desperate Rom LP UX4 V4 Player/LG Player 1.0 for Android 5.0.2 (stagefright alternative)" do
+  before do
+    @useragent = UserAgent.parse("LG-D802/Desperate Rom LP UX4 V4 Player/LG Player 1.0 for Android 5.0.2 (stagefright alternative)")
+  end
+
+  it_behaves_like 'LG Player'
+
+  it "returns '1.0' as its version" do
+    expect(@useragent.version).to eq('1.0')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+
+  it "returns 'Android 5.0.2' as its operating system" do
+    expect(@useragent.os).to eq('Android 5.0.2')
+  end
+end
+
+describe "UserAgent: LG-H873/V15c Player/LG Player 1.0 for Android 7.0 (stagefright alternative)" do
+  before do
+    @useragent = UserAgent.parse("LG-H873/V15c Player/LG Player 1.0 for Android 7.0 (stagefright alternative)")
+  end
+
+  it_behaves_like 'LG Player'
+
+  it "returns '1.0' as its version" do
+    expect(@useragent.version).to eq('1.0')
+  end
+
+  it "returns 'Android' as its platform" do
+    expect(@useragent.platform).to eq('Android')
+  end
+
+  it "returns 'Android 7.0' as its operating system" do
+    expect(@useragent.os).to eq('Android 7.0')
+  end
+end
+
+describe "UserAgent: LG-H955/V15c Player/LG Player 1.0 for Android 5.1.1 (stagefright alternative) BMID/E67AB269FB" do
+  before do
+    @useragent = UserAgent.parse("LG-H955/V15c Player/LG Player 1.0 for Android 5.1.1 (stagefright alternative) BMID/E67AB269FB")
+  end
+
+  it_behaves_like 'LG Player'
+
+  it "returns '1.0' as its version" do
+    expect(@useragent.version).to eq('1.0')
   end
 
   it "returns 'Android' as its platform" do
