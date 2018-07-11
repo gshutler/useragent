26a3850d4837d70d6e4ad3131794c7c4e17dd8f5
SiuKit Wong <skwongr@gmail.com>
2018-07-11 15:47:08 -0500

    AROR-1578 Support OkHttp user agent

---
+++ lib/user_agent/browsers/ok_http.rb
@@ -0,0 +1,53 @@
+class UserAgent
+  module Browsers
+    # okhttp/3.8.1
+    # okhttp/3.10.0 BMID/E67925BB27
+    # okhttp/3.8.1 Dalvik/2.1.0 (Linux; U; Android 7.1.1; vivo X20A Build/NMF26X) baiduboxapp/10.1.0.11 (Baidu; P1 7.1.1)
+    # okhttp/2.7.5 nyt-android/6.19.3 ,okhttp/2.7.5 nyt-android/6.19.3
+    class OkHttp < Base
+      OKHTTP_PRODUCT = 'okhttp'
+
+      def self.extend?(agent)
+        agent.detect { |useragent| useragent.product == OKHTTP_PRODUCT }
+      end
+
+      def browser
+        'OkHttp'
+      end
+
+      # Find the right application
+      def application
+        self.reject { |agent| agent.comment.nil? || agent.comment.empty? }.first
+      end
+
+      # Return the platform
+      #
+      # @return [String]
+      def platform
+        'Android'
+      end
+
+      # Return the operating system
+      #
+      # @return [String]
+      def os
+        return unless application
+
+        if application.comment[2].nil?
+          OperatingSystems.normalize_os(application.comment[1])
+        elsif application.comment[1] =~ /Android/
+          OperatingSystems.normalize_os(application.comment[1])
+        else
+          OperatingSystems.normalize_os(application.comment[2])
+        end
+      end
+
+      # Return the application version if any
+      #
+      # @return [String]
+      def version
+        detect_product(OKHTTP_PRODUCT).version
+      end
+    end
+  end
+end
