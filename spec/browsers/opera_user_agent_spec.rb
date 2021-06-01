94a79bd7491f20887d27e6147555a576f0a40b1a
SiuKit Wong <kit@art19.com>
2021-06-01 17:01:41 -0500

    update Opera

--- spec/browsers/opera_user_agent_spec.rb
+++ spec/browsers/opera_user_agent_spec.rb
@@ -1,6 +1,6 @@
 require 'user_agent'
 
-shared_examples_for "Opera browser" do
+shared_examples_for "Opera browser" do |type|
   it "should return 'Opera' as its browser" do
     expect(@useragent.browser).to eq("Opera")
   end
@@ -8,6 +8,20 @@ shared_examples_for "Opera browser" do
   it "should return a Version object for version" do
     expect(@useragent.version).to be_a(UserAgent::Version)
   end
+
+  if type == :desktop
+    it { expect(@useragent).to be_desktop }
+    it { expect(@useragent).not_to be_mobile }
+  elsif type == :mobile
+    it { expect(@useragent).to be_mobile }
+    it { expect(@useragent).not_to be_desktop }
+  else
+    it { expect(@useragent).not_to be_mobile }
+    it { expect(@useragent).not_to be_desktop }
+  end
+
+  it { expect(@useragent).not_to be_speaker }
+  it { expect(@useragent).not_to be_bot }
 end
 
 # http://www.useragentstring.com/Opera12.14_id_19612.php
@@ -16,7 +30,7 @@ describe "UserAgent: 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'
     @useragent = UserAgent.parse("Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14")
   end
 
-  it_should_behave_like "Opera browser"
+  it_behaves_like 'Opera browser', :desktop
 
   it "should return '12.14' as its version" do
     expect(@useragent.version).to eq("12.14")
@@ -29,8 +43,6 @@ describe "UserAgent: 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'
   it "should return 'Windows Vista' as its os" do
     expect(@useragent.os).to eq("Windows Vista")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Opera/9.27 (Macintosh; Intel Mac OS X; U; en)'" do
@@ -38,7 +50,7 @@ describe "UserAgent: 'Opera/9.27 (Macintosh; Intel Mac OS X; U; en)'" do
     @useragent = UserAgent.parse("Opera/9.27 (Macintosh; Intel Mac OS X; U; en)")
   end
 
-  it_should_behave_like "Opera browser"
+  it_behaves_like 'Opera browser', :desktop
 
   it "should return '9.27' as its version" do
     expect(@useragent.version).to eq("9.27")
@@ -55,8 +67,6 @@ describe "UserAgent: 'Opera/9.27 (Macintosh; Intel Mac OS X; U; en)'" do
   it "should return 'en' as its localization" do
     expect(@useragent.localization).to eq("en")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Opera/9.27 (Windows NT 5.1; U; en)'" do
@@ -64,7 +74,7 @@ describe "UserAgent: 'Opera/9.27 (Windows NT 5.1; U; en)'" do
     @useragent = UserAgent.parse("Opera/9.27 (Windows NT 5.1; U; en)")
   end
 
-  it_should_behave_like "Opera browser"
+  it_behaves_like 'Opera browser', :desktop
 
   it "should return '9.27' as its version" do
     expect(@useragent.version).to eq("9.27")
@@ -81,8 +91,6 @@ describe "UserAgent: 'Opera/9.27 (Windows NT 5.1; U; en)'" do
   it "should return 'en' as its localization" do
     expect(@useragent.localization).to eq("en")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Opera/9.80'" do
@@ -107,8 +115,6 @@ describe "UserAgent: 'Opera/9.80'" do
   it "should return nil as its localization" do
     expect(@useragent.localization).to be_nil
   end
-
-  it { expect(@useragent).not_to be_desktop }
 end
 
 describe "UserAgent: 'Opera/9.80 (Macintosh; Intel Mac OS X; U; en) Presto/2.2.15 Version/10.10'" do
@@ -116,7 +122,7 @@ describe "UserAgent: 'Opera/9.80 (Macintosh; Intel Mac OS X; U; en) Presto/2.2.1
     @useragent = UserAgent.parse("Opera/9.80 (Macintosh; Intel Mac OS X; U; en) Presto/2.2.15 Version/10.10")
   end
 
-  it_should_behave_like "Opera browser"
+  it_behaves_like 'Opera browser', :desktop
 
   it "should return '10.10' as its version" do
     expect(@useragent.version).to eq("10.10")
@@ -133,8 +139,6 @@ describe "UserAgent: 'Opera/9.80 (Macintosh; Intel Mac OS X; U; en) Presto/2.2.1
   it "should return 'en' as its localization" do
     expect(@useragent.localization).to eq("en")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Opera/9.80 (Windows NT 6.0; U; en) Presto/2.2.15 Version/10.10'" do
@@ -142,7 +146,7 @@ describe "UserAgent: 'Opera/9.80 (Windows NT 6.0; U; en) Presto/2.2.15 Version/1
     @useragent = UserAgent.parse("Opera/9.80 (Windows NT 6.0; U; en) Presto/2.2.15 Version/10.10")
   end
 
-  it_should_behave_like "Opera browser"
+  it_behaves_like 'Opera browser', :desktop
 
   it "should return '10.10' as its version" do
     expect(@useragent.version).to eq("10.10")
@@ -159,8 +163,6 @@ describe "UserAgent: 'Opera/9.80 (Windows NT 6.0; U; en) Presto/2.2.15 Version/1
   it "should return 'en' as its localization" do
     expect(@useragent.localization).to eq("en")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Opera/9.80 (J2ME/MIDP; Opera Mini/9 (Compatible; MSIE:9.0; iPhone; BlackBerry9700; AppleWebKit/24.746; U; en) Presto/2.5.25 Version/10.54'" do
@@ -168,10 +170,7 @@ describe "UserAgent: 'Opera/9.80 (J2ME/MIDP; Opera Mini/9 (Compatible; MSIE:9.0;
     @useragent = UserAgent.parse("Opera/9.80 (J2ME/MIDP; Opera Mini/9 (Compatible; MSIE:9.0; iPhone; BlackBerry9700; AppleWebKit/24.746; U; en) Presto/2.5.25 Version/10.54'")
   end
 
-  it_should_behave_like "Opera browser"
-
-  it { expect(@useragent).to be_mobile }
-  it { expect(@useragent).not_to be_desktop }
+  it_behaves_like 'Opera browser', :mobile
 end
 
 describe "UserAgent: 'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (J2ME/23.377; U; en) Presto/2.5.25 Version/10.54'" do
@@ -179,10 +178,7 @@ describe "UserAgent: 'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (J2ME/23.377; U; en
     @useragent = UserAgent.parse("Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (J2ME/23.377; U; en) Presto/2.5.25 Version/10.54")
   end
 
-  it_should_behave_like "Opera browser"
-
-  it { expect(@useragent).to be_mobile }
-  it { expect(@useragent).not_to be_desktop }
+  it_behaves_like 'Opera browser', :mobile
 end
 
 # http://www.useragentstring.com/Opera%20Mini9.80_id_17936.php
@@ -191,13 +187,11 @@ describe "UserAgent: 'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera
     @useragent = UserAgent.parse("Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54")
   end
 
-  it_should_behave_like "Opera browser"
+  it_behaves_like 'Opera browser', :mobile
 
   it "should return '9.80' as its version" do
     expect(@useragent.version).to eq("9.80")
   end
-
-  it { expect(@useragent).not_to be_desktop }
 end
 
 # http://www.useragentstring.com/Opera%20Mini7.1.32694_id_19147.php
@@ -206,13 +200,11 @@ describe "UserAgent: 'Opera/9.80 (iPhone; Opera Mini/7.1.32694/27.1407; U; en) P
     @useragent = UserAgent.parse("Opera/9.80 (iPhone; Opera Mini/7.1.32694/27.1407; U; en) Presto/2.8.119 Version/11.10")
   end
 
-  it_should_behave_like "Opera browser"
+  it_behaves_like 'Opera browser', :mobile
 
   it "should return '7.1.32694' as its version" do
     expect(@useragent.version).to eq("7.1.32694")
   end
-
-  it { expect(@useragent).not_to be_desktop }
 end
 
 describe "Opera/9.80 (BREW Opera Mini/6.0.3/27.2354 U es) Presto/2.8.119 240X320 Samsung SCH-U485" do
@@ -220,13 +212,11 @@ describe "Opera/9.80 (BREW Opera Mini/6.0.3/27.2354 U es) Presto/2.8.119 240X320
     @useragent = UserAgent.parse("Opera/9.80 (BREW Opera Mini/6.0.3/27.2354 U es) Presto/2.8.119 240X320 Samsung SCH-U485")
   end
 
-  it_should_behave_like "Opera browser"
+  it_behaves_like 'Opera browser', :mobile
 
   it "should return '6.0.3' as its version" do
     expect(@useragent.version).to eq("6.0.3")
   end
-
-  it { expect(@useragent).not_to be_desktop }
 end
 
 describe "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36 OPR/28.0.1750.48" do
@@ -234,11 +224,9 @@ describe "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)
     @useragent = UserAgent.parse("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36 OPR/28.0.1750.48")
   end
 
-  it_should_behave_like "Opera browser"
+  it_behaves_like 'Opera browser', :desktop
 
   it "should return '28.0.1750.48' as it's version" do
     expect(@useragent.version).to eq("28.0.1750.48")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
