1c4fb2795d9f37cf805a2b741c3e3f79a4e8b23a
SiuKit Wong <kit@art19.com>
2021-06-01 17:04:46 -0500

    update Overcast

--- spec/browsers/overcast_user_agent_spec.rb
+++ spec/browsers/overcast_user_agent_spec.rb
@@ -1,6 +1,6 @@
 require 'user_agent'
 
-shared_examples_for 'Overcast' do |version, platform, os, type|
+shared_examples 'Overcast' do |version, platform, os, type|
   it "returns 'Overcast' as its browser" do
     expect(useragent.browser).to eq('Overcast')
   end
@@ -19,31 +19,36 @@ shared_examples_for 'Overcast' do |version, platform, os, type|
 
   if type == :mobile
     it { expect(useragent).to be_mobile }
+    it { expect(useragent).not_to be_bot }
   elsif type == :bot
     it { expect(useragent).to be_bot }
+    it { expect(useragent).not_to be_mobile }
   end
+
+  it { expect(useragent).not_to be_desktop }
+  it { expect(useragent).not_to be_speaker }
 end
 
 describe "UserAgent: Overcast/3.0 (+http://overcast.fm/; iOS podcast app) BMID/E6793162B9" do
   let!(:useragent) { UserAgent.parse("Overcast/3.0 (+http://overcast.fm/; iOS podcast app) BMID/E6793162B9") }
 
-  it_should_behave_like 'Overcast', '3.0', 'iOS', nil, :mobile
+  it_behaves_like 'Overcast', '3.0', 'iOS', nil, :mobile
 end
 
 describe "UserAgent: Overcast (+http://overcast.fm/; Apple Watch podcast app)" do
   let!(:useragent) { UserAgent.parse("Overcast (+http://overcast.fm/; Apple Watch podcast app)") }
 
-  it_should_behave_like 'Overcast', '', 'Apple Watch', nil, :mobile
+  it_behaves_like 'Overcast', '', 'Apple Watch', nil, :mobile
 end
 
 describe "UserAgent: Overcast/857 CFNetwork/1209 Darwin/20.3.0" do
   let!(:useragent) { UserAgent.parse("Overcast/857 CFNetwork/1209 Darwin/20.3.0") }
 
-  it_should_behave_like 'Overcast', '857', 'iOS', 'iOS 14.4.x', :mobile
+  it_behaves_like 'Overcast', '857', 'iOS', 'iOS 14.4.x', :mobile
 end
 
 describe "UserAgent: Overcast/1.0 Podcast Sync (+http://overcast.fm/)" do
   let!(:useragent) { UserAgent.parse("Overcast/1.0 Podcast Sync (+http://overcast.fm/)") }
 
-  it_should_behave_like 'Overcast', '1.0', nil, nil, :bot
+  it_behaves_like 'Overcast', '1.0', nil, nil, :bot
 end
