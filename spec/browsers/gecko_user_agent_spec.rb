819cfe22293d4b290ec7d1c49fc2fe827de8d88d
SiuKit Wong <kit@art19.com>
2021-06-01 16:10:40 -0500

    update gecko spec

--- spec/browsers/gecko_user_agent_spec.rb
+++ spec/browsers/gecko_user_agent_spec.rb
@@ -10,12 +10,27 @@ shared_examples_for "Firefox browser" do
   end
 end
 
+shared_examples 'a desktop' do
+  it { expect(@useragent).to be_desktop }
+  it { expect(@useragent).not_to be_mobile }
+  it { expect(@useragent).not_to be_speaker }
+  it { expect(@useragent).not_to be_bot }
+end
+
+shared_examples 'a mobile' do
+  it { expect(@useragent).to be_mobile }
+  it { expect(@useragent).not_to be_desktop }
+  it { expect(@useragent).not_to be_speaker }
+  it { expect(@useragent).not_to be_bot }
+end
+
 describe 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0b8) Gecko/20100101 Firefox/4.0b8' do
   before do
     @useragent = UserAgent.parse('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0b8) Gecko/20100101 Firefox/4.0b8')
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '4.0b8' as its version" do
     expect(@useragent.version).to eq("4.0b8")
@@ -36,9 +51,6 @@ describe 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0b8) Gecko/20100101 
   it "should return nil as its localization" do
     expect(@useragent.localization).to be_nil
   end
-
-  it { expect(@useragent).not_to be_mobile }
-  it { expect(@useragent).to be_desktop }
 end
 
 describe 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13' do
@@ -46,7 +58,8 @@ describe 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Ge
     @useragent = UserAgent.parse('Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13')
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '3.6.13' as its version" do
     expect(@useragent.version).to eq("3.6.13")
@@ -67,9 +80,6 @@ describe 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Ge
   it "should return 'en-US' as its localization" do
     expect(@useragent.localization).to eq("en-US")
   end
-
-  it { expect(@useragent).not_to be_mobile }
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008070206 Firefox/3.0.1'" do
@@ -77,7 +87,8 @@ describe "UserAgent: 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/
     @useragent = UserAgent.parse("Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008070206 Firefox/3.0.1")
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '3.0.1' as its version" do
     expect(@useragent.version).to eq("3.0.1")
@@ -98,9 +109,6 @@ describe "UserAgent: 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/
   it "should return 'en-US' as its localization" do
     expect(@useragent.localization).to eq("en-US")
   end
-
-  it { expect(@useragent).not_to be_mobile }
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (X11; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0'" do
@@ -108,7 +116,8 @@ describe "UserAgent: 'Mozilla/5.0 (X11; Linux x86_64; rv:27.0) Gecko/20100101 Fi
     @useragent = UserAgent.parse("Mozilla/5.0 (X11; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0")
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '27.0' as its version" do
     expect(@useragent.version).to eq("27.0")
@@ -125,9 +134,6 @@ describe "UserAgent: 'Mozilla/5.0 (X11; Linux x86_64; rv:27.0) Gecko/20100101 Fi
   it "should return 'Linux x86_64' as its os" do
     expect(@useragent.os).to eq("Linux x86_64")
   end
-
-  it { expect(@useragent).not_to be_mobile }
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14'" do
@@ -135,7 +141,8 @@ describe "UserAgent: 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US; rv:1.8.1
     @useragent = UserAgent.parse("Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14")
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '2.0.0.14' as its version" do
     expect(@useragent.version).to eq("2.0.0.14")
@@ -156,8 +163,6 @@ describe "UserAgent: 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US; rv:1.8.1
   it "should return 'en-US' as its localization" do
     expect(@useragent.localization).to eq("en-US")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14'" do
@@ -165,7 +170,8 @@ describe "UserAgent: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1
     @useragent = UserAgent.parse("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14")
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '2.0.0.14' as its version" do
     expect(@useragent.version).to eq("2.0.0.14")
@@ -186,8 +192,6 @@ describe "UserAgent: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1
   it "should return 'en-US' as its localization" do
     expect(@useragent.localization).to eq("en-US")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1'" do
@@ -195,7 +199,8 @@ describe "UserAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0.1) Gecko/20121
     @useragent = UserAgent.parse("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1")
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '16.0.1' as its version" do
     expect(@useragent.version).to eq("16.0.1")
@@ -216,8 +221,6 @@ describe "UserAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0.1) Gecko/20121
   it "should return nil as its localization" do
     expect(@useragent.localization).to be_nil
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (Windows NT 6.1; Win64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1'" do
@@ -225,7 +228,8 @@ describe "UserAgent: 'Mozilla/5.0 (Windows NT 6.1; Win64; rv:16.0.1) Gecko/20121
     @useragent = UserAgent.parse("Mozilla/5.0 (Windows NT 6.1; Win64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1")
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '16.0.1' as its version" do
     expect(@useragent.version).to eq("16.0.1")
@@ -246,8 +250,6 @@ describe "UserAgent: 'Mozilla/5.0 (Windows NT 6.1; Win64; rv:16.0.1) Gecko/20121
   it "should return nil as its localization" do
     expect(@useragent.localization).to be_nil
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe 'Mozilla/5.0 (Windows NT 5.1; rv:17.0) Gecko/20100101 Firefox/17.0' do
@@ -255,7 +257,8 @@ describe 'Mozilla/5.0 (Windows NT 5.1; rv:17.0) Gecko/20100101 Firefox/17.0' do
     @useragent = UserAgent.parse('Mozilla/5.0 (Windows NT 5.1; rv:17.0) Gecko/20100101 Firefox/17.0')
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '17.0' as its version" do
     expect(@useragent.version).to eq("17.0")
@@ -276,8 +279,6 @@ describe 'Mozilla/5.0 (Windows NT 5.1; rv:17.0) Gecko/20100101 Firefox/17.0' do
   it "should return nil as its localization" do
     expect(@useragent.localization).to be_nil
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe 'UserAgent: Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0' do
@@ -285,7 +286,8 @@ describe 'UserAgent: Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefo
     @useragent = UserAgent.parse('Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0')
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '17.0' as its version" do
     expect(@useragent.version).to eq("17.0")
@@ -306,8 +308,6 @@ describe 'UserAgent: Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefo
   it "should return nil as its localization" do
     expect(@useragent.localization).to be_nil
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.0.12) Gecko/20070508 Firefox/1.5.0.12'" do
@@ -315,7 +315,8 @@ describe "UserAgent: 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:
     @useragent = UserAgent.parse("Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.0.12) Gecko/20070508 Firefox/1.5.0.12")
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '1.5.0.12' as its version" do
     expect(@useragent.version).to eq("1.5.0.12")
@@ -336,8 +337,6 @@ describe "UserAgent: 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:
   it "should return 'en-US' as its localization" do
     expect(@useragent.localization).to eq("en-US")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.12) Gecko/20070508 Firefox/1.5.0.12'" do
@@ -345,7 +344,8 @@ describe "UserAgent: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.1
     @useragent = UserAgent.parse("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.12) Gecko/20070508 Firefox/1.5.0.12")
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '1.5.0.12' as its version" do
     expect(@useragent.version).to eq("1.5.0.12")
@@ -366,8 +366,6 @@ describe "UserAgent: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.1
   it "should return 'en-US' as its localization" do
     expect(@useragent.localization).to eq("en-US")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.4) Gecko/20060612 Firefox/1.5.0.4 Flock/0.7.0.17.1'" do
@@ -375,7 +373,8 @@ describe "UserAgent: 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.4) Gecko/
     @useragent = UserAgent.parse("Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.4) Gecko/20060612 Firefox/1.5.0.4 Flock/0.7.0.17.1")
   end
 
-  it_should_behave_like "Firefox browser"
+  it_behaves_like 'Firefox browser'
+  it_behaves_like 'a desktop'
 
   it "should return '1.5.0.4' as its version" do
     expect(@useragent.version).to eq("1.5.0.4")
@@ -396,14 +395,14 @@ describe "UserAgent: 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.4) Gecko/
   it "should return 'en-US' as its localization" do
     expect(@useragent.localization).to eq("en-US")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en; rv:1.8.1.14) Gecko/20080409 Camino/1.6 (like Firefox/2.0.0.14)'" do
   before do
     @useragent = UserAgent.parse("Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en; rv:1.8.1.14) Gecko/20080409 Camino/1.6 (like Firefox/2.0.0.14)")
   end
+
+  it_behaves_like 'a desktop'
 
   it "should return 'Camino' as its browser" do
     expect(@useragent.browser).to eq("Camino")
@@ -428,14 +427,14 @@ describe "UserAgent: 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en; rv:1.8.1.14
   it "should return 'en' as its localization" do
     expect(@useragent.localization).to eq("en")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe 'UserAgent: Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1) Gecko/20061024 Iceweasel/2.0 (Debian-2.0+dfsg-1)' do
   before do
     @useragent = UserAgent.parse('Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1) Gecko/20061024 Iceweasel/2.0 (Debian-2.0+dfsg-1)')
   end
+
+  it_behaves_like 'a desktop'
 
   it "should return 'Iceweasel' as its browser" do
     expect(@useragent.browser).to eq("Iceweasel")
@@ -460,14 +459,14 @@ describe 'UserAgent: Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1) Gecko/200
   it "should return 'en-US' as its localization" do
     expect(@useragent.localization).to eq("en-US")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe 'UserAgent: Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.1.4) Gecko/20091017 SeaMonkey/2.0' do
   before do
     @useragent = UserAgent.parse('Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.1.4) Gecko/20091017 SeaMonkey/2.0')
   end
+
+  it_behaves_like 'a desktop'
 
   it "should return 'Seamonkey' as its browser" do
     expect(@useragent.browser).to eq("Seamonkey")
@@ -492,8 +491,6 @@ describe 'UserAgent: Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1
   it "should return 'en-US' as its localization" do
     expect(@useragent.localization).to eq("en-US")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe 'Mozilla/5.0 (Android; Mobile; rv:19.0) Gecko/19.0 Firefox/19.0' do
@@ -502,10 +499,7 @@ describe 'Mozilla/5.0 (Android; Mobile; rv:19.0) Gecko/19.0 Firefox/19.0' do
   end
 
   it_behaves_like 'Firefox browser'
-
-  it 'returns true for mobile?' do
-    expect(@useragent.mobile?).to be true
-  end
+  it_behaves_like 'a mobile'
 
   it "returns 'Android' as the platform" do
     expect(@useragent.platform).to eq('Android')
@@ -514,8 +508,6 @@ describe 'Mozilla/5.0 (Android; Mobile; rv:19.0) Gecko/19.0 Firefox/19.0' do
   it "returns 'Android' as the operating system" do
     expect(@useragent.os).to eq('Android')
   end
-
-  it { expect(@useragent).not_to be_desktop }
 end
 
 describe 'Mozilla/5.0 (Android 8.0.0; Mobile; rv:66.0) Gecko/66.0 Firefox/66.0' do
@@ -524,10 +516,7 @@ describe 'Mozilla/5.0 (Android 8.0.0; Mobile; rv:66.0) Gecko/66.0 Firefox/66.0' 
   end
 
   it_behaves_like 'Firefox browser'
-
-  it 'returns true for mobile?' do
-    expect(@useragent.mobile?).to be true
-  end
+  it_behaves_like 'a mobile'
 
   it "returns 'Android' as the platform" do
     expect(@useragent.platform).to eq('Android')
@@ -536,8 +525,6 @@ describe 'Mozilla/5.0 (Android 8.0.0; Mobile; rv:66.0) Gecko/66.0 Firefox/66.0' 
   it "returns 'Android 8.0.0' as the operating system" do
     expect(@useragent.os).to eq('Android 8.0.0')
   end
-
-  it { expect(@useragent).not_to be_desktop }
 end
 
 describe 'Mozilla/5.0 (Mobile; rv:41.0) Gecko/41.0 Firefox/41.0' do
@@ -546,10 +533,7 @@ describe 'Mozilla/5.0 (Mobile; rv:41.0) Gecko/41.0 Firefox/41.0' do
   end
 
   it_behaves_like 'Firefox browser'
-
-  it 'returns true for mobile?' do
-    expect(@useragent.mobile?).to be true
-  end
+  it_behaves_like 'a mobile'
 
   it "returns nil as the platform" do
     expect(@useragent.platform).to be_nil
@@ -558,8 +542,6 @@ describe 'Mozilla/5.0 (Mobile; rv:41.0) Gecko/41.0 Firefox/41.0' do
   it "returns nil as the operating system" do
     expect(@useragent.os).to be_nil
   end
-
-  it { expect(@useragent).not_to be_desktop }
 end
 
 describe 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.x.x) Gecko/20041107 Firefox/x.x' do
@@ -568,6 +550,7 @@ describe 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.x.x) Gecko/200411
   end
 
   it_should_behave_like "Firefox browser"
+  it_behaves_like 'a desktop'
 
   it "should return 'x.x' as its version" do
     expect(@useragent.version).to eq("x.x")
@@ -584,8 +567,6 @@ describe 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.x.x) Gecko/200411
   it "should return 'Windows XP' as its os" do
     expect(@useragent.os).to eq("Windows XP")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 shared_examples_for "PaleMoon browser" do
@@ -599,7 +580,8 @@ describe 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.7) Gecko/20140802 Firefox/24
     @useragent = UserAgent.parse('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.7) Gecko/20140802 Firefox/24.7 PaleMoon/24.7.1')
   end
 
-  it_should_behave_like "PaleMoon browser"
+  it_behaves_like 'PaleMoon browser'
+  it_behaves_like 'a desktop'
 
   it "should return '24.7.1' as its version" do
     expect(@useragent.version).to eq("24.7.1")
@@ -620,8 +602,6 @@ describe 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.7) Gecko/20140802 Firefox/24
   it "should return 'Windows 7' as its os" do
     expect(@useragent.os).to eq("Windows 7")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20141001 PaleMoon/25.0.0' do
@@ -629,7 +609,8 @@ describe 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20141001 PaleMoon/2
     @useragent = UserAgent.parse('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20141001 PaleMoon/25.0.0')
   end
 
-  it_should_behave_like "PaleMoon browser"
+  it_behaves_like 'PaleMoon browser'
+  it_behaves_like 'a desktop'
 
   it "should return '25.0.0' as its version" do
     expect(@useragent.version).to eq("25.0.0")
@@ -642,6 +623,4 @@ describe 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20141001 PaleMoon/2
   it "should return '24.7' as its firefox version" do
     expect { @useragent.firefox }.to raise_error(NoMethodError)
   end
-
-  it { expect(@useragent).to be_desktop }
 end
