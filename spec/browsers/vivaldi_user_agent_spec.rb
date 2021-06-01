53a81ab205782b7a300c63768a9a6ca77a6c4d4e
SiuKit Wong <kit@art19.com>
2021-06-01 17:53:11 -0500

    update Vivaldi

--- spec/browsers/vivaldi_user_agent_spec.rb
+++ spec/browsers/vivaldi_user_agent_spec.rb
@@ -4,6 +4,11 @@ shared_examples_for "Vivaldi browser" do
   it "should return 'Vivaldi' as its browser" do
     expect(@useragent.browser).to eq("Vivaldi")
   end
+
+  it { expect(@useragent).to be_desktop }
+  it { expect(@useragent).not_to be_mobile }
+  it { expect(@useragent).not_to be_speaker }
+  it { expect(@useragent).not_to be_bot }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 Vivaldi/1.2.490.43'" do
@@ -24,8 +29,6 @@ describe "UserAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KH
   it "should return 'Windows 7' as its os" do
     expect(@useragent.os).to eq("Windows 7")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
 
 describe "UserAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 Vivaldi/1.2.490.43'" do
@@ -46,6 +49,4 @@ describe "UserAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,
   it "should return 'Linux x86_64' as its os" do
     expect(@useragent.os).to eq("Linux x86_64")
   end
-
-  it { expect(@useragent).to be_desktop }
 end
