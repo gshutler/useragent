a36811313f4f120ecd3f5ea0d4bf5efe3300cec5
SiuKit Wong <kit@art19.com>
2021-06-23 10:05:28 -0500

    support Himalaya

--- lib/user_agent/browsers.rb
+++ lib/user_agent/browsers.rb
@@ -28,6 +28,7 @@ require 'user_agent/browsers/gecko'
 require 'user_agent/browsers/google_home'
 require 'user_agent/browsers/google_podcasts'
 require 'user_agent/browsers/googlebot'
+require 'user_agent/browsers/himalaya'
 require 'user_agent/browsers/iheart_radio'
 require 'user_agent/browsers/internet_explorer'
 require 'user_agent/browsers/itunes'
@@ -84,6 +85,7 @@ class UserAgent
       Alexa,
       AmazonMusic,
       Deezer,
+      Himalaya,
       PocketCasts,
       Podbean,
       PodcastAddict,
